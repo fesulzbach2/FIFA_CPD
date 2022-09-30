799b8b6c1eee4b02473d91446524cfa17cb67ce6
fesulzbach2 <92560535+fesulzbach2@users.noreply.github.com>
2022-09-30 17:17:00 -0300

    Add files via upload

---
+++ FIFA21.cpp
@@ -0,0 +1,239 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <list>
+#include <cstring>
+
+#define TABLE_SIZE 20000
+
+using namespace std;
+
+
+struct PlayerList {
+public:
+    PlayerList(
+        int id,
+        string playerName,
+        string positions,
+        PlayerList *next
+
+    ) {
+        Id = id;
+        PlayerName = playerName;
+        Positions = positions;
+    }
+
+    void display() {
+        cout << setw(10) << left << Id;
+        cout << setw(40) << left << PlayerName;
+        cout << setw(25) << left << Positions;
+        cout << left << average/Asize << " ";
+        cout << endl;
+    }
+
+    int Id;
+    string PlayerName;
+    string Positions;
+   // vector<float> ratings;
+    float average=0;
+    int Asize=0;
+    PlayerList *next;
+
+};
+
+struct Avaliations
+{
+    PlayerList *players;
+    float rating;
+};
+
+struct Users
+{
+    int userID;
+    vector<Avaliations> ratedPlayers;
+};
+
+PlayerList *hashTable[TABLE_SIZE];
+vector<PlayerList> players;
+vector<Users> users;
+
+unsigned int hash_(int id)
+{
+    int hash_value=0;
+
+        hash_value = (3*id^2)%TABLE_SIZE;
+
+    return hash_value;
+}
+
+void init_hashTable()
+{
+    for(int i=0;i<TABLE_SIZE;i++){
+        hashTable[i]=NULL;
+
+    }
+}
+
+void print_table()
+{
+    cout << "  START" << endl;
+    for(int i=0;i<TABLE_SIZE;i++){
+        if(hashTable[i]==NULL){
+            cout << i << "  ---";
+
+        } else{
+        cout << i << " ";};
+        PlayerList *tmp = hashTable[i];
+        while(tmp != NULL){
+            cout << tmp->PlayerName << " -> ";
+            tmp = tmp->next;
+        }
+        printf("\n");
+    }
+    cout << "  END\n" << endl;
+}
+
+bool hash_table_insert(PlayerList *p)
+{
+    if(p == NULL)
+        return false;
+
+    int index = hash_(p->Id);
+    p->next = hashTable[index];
+    hashTable[index] = p;
+    return true;
+
+
+}
+
+PlayerList *hash_table_search(int number_id)
+{
+    int index = hash_(number_id);
+    PlayerList *tmp = hashTable[index];
+    while(tmp!= NULL && (tmp->Id != number_id)){
+        tmp = tmp->next;
+    }
+    return tmp;
+}
+
+
+/*void displayStudents(vector<PlayerList> &players) {
+
+    for (auto student : players) {
+        student.display();
+    }
+}
+*/
+
+void open_players()
+{
+    ifstream inputFile;
+    inputFile.open("players.csv");
+    string line = "";
+    int counter=0;
+
+
+
+    while (getline(inputFile, line)) {
+
+        stringstream inputString(line);
+
+        //StudentId, Last Name, playerName, Age, Phone Number, GPA
+        int playerID;
+        string playerName;
+        string positions;
+        string tempString;
+
+
+        getline(inputString, tempString, ',');
+        playerID = atoi(tempString.c_str());
+        getline(inputString,playerName, ',');
+        getline(inputString,positions, '\n');
+
+        PlayerList student(playerID, playerName, positions, NULL);
+        players.push_back(student);
+        line = "";
+        counter++;
+    }
+
+    init_hashTable();
+
+    for(int i=0;i<counter;i++)
+    hash_table_insert(&players[i]);
+   // print_table();
+
+}
+
+void open_ratings()
+{
+    ifstream inputFile;
+    inputFile.open("minirating.csv");
+    string line = "";
+    int counter=0;
+
+   // vector<PlayerList> players;
+    while (getline(inputFile, line)) {
+
+        stringstream inputString(line);
+
+        //StudentId, Last Name, playerName, Age, Phone Number, GPA
+        int userID;
+        int playerID;
+        float rating=0;
+        string tempString;
+
+        getline(inputString, tempString, ',');
+        userID = atoi(tempString.c_str());
+
+        getline(inputString, tempString, ',');
+        playerID = atoi(tempString.c_str());
+
+        getline(inputString, tempString, '\n');
+        rating = atof(tempString.c_str());
+
+
+       // PlayerList student(playerID, playerName, lastName, NULL);
+        hash_table_search(playerID)->average = hash_table_search(playerID)->average + rating;
+        hash_table_search(playerID)->Asize++;
+
+        line = "";
+     //   counter++;
+    }
+
+}
+
+
+
+
+int main()
+{
+
+    open_players();
+    open_ratings();
+
+    //print_table();
+
+
+   /* PlayerList *tmp1 = hash_table_search(players[1].Id);
+    PlayerList *tmp2 = hash_table_search(players[2].Id);
+
+    if(tmp1 == NULL){
+        printf("Not found!\n");
+    }else
+        tmp1->display();
+
+
+        if(tmp2 == NULL){
+        printf("Not found!\n");
+    }else
+        tmp2->display();*/
+
+        for (auto players : players)
+            players.display();
+
+
+    //displayStudents(players);
+}
